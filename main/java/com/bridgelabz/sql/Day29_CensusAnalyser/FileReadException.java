0f9958ac1e69cdbeaeb052378a5068fd3e690097
Bharti <bharatifule10gmail.com>
2023-02-28 00:24:54 +0530

    Added:Sensus-Analyser

---
+++ main/java/com/bridgelabz/sql/Day29_CensusAnalyser/FileReadException.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.sql.Day29_CensusAnalyser;
+
+public class FileReadException extends Exception {
+    public FileReadException (String str)
+    {
+        // calling the constructor of parent Exception
+        super(str);
+    }
+
+}
--- test/java/com/bridgelabz/sql/Day29_CensusAnalyser/TestCSVStateCensus.java
+++ test/java/com/bridgelabz/sql/Day29_CensusAnalyser/TestCSVStateCensus.java
@@ -8,16 +8,27 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class TestCSVStateCensus {
-	 static CSVStateCensus csvStateCensus;
-	    @Test
-	    public void testReadDataFromDestinationHappy(){
-	        csvStateCensus =  new CSVStateCensus();
-	        try{
-	            ArrayList<String> stateList = csvStateCensus.readDataFromSource();
-	            Assertions.assertEquals(stateList.size(), 199);
-	        } catch(Exception e){
-	            fail("Should not throw exception");
-	        }
-	    }
+	static CSVStateCensus csvStateCensus;
+    @Test
+    public void testReadDataFromDestinationHappy(){
+        csvStateCensus =  new CSVStateCensus();
+        try{
+            ArrayList<String> stateList = csvStateCensus.readDataFromSource("C:\\Day_29_Census_Analyzer\\src\\main\\java\\com\\bridgelabz\\State_census.csv");
+            Assertions.assertEquals(stateList.size(), 199);
+        } catch(Exception e){
+            fail("Should not throw exception");
+        }
+    }
+
+    @Test
+    public void testReadDataFromDestinationSadWrongFile(){
+        csvStateCensus =  new CSVStateCensus();
+        try{
+            ArrayList<String> stateList = csvStateCensus.readDataFromSource("C:\\Day_29_Census_Analyzer\\src\\main\\java\\com\\bridgelabz\\State.csv");
+            fail("Should throw exception");
+        } catch(Exception e){
+            Assertions.assertNotNull(e.getMessage());
+        }
+    }
 
 }
