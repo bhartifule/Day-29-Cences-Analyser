97bc7160c6ee92bd52e6806ac0fc3b3391d02285
Bharti <bharatifule10gmail.com>
2023-02-28 17:47:50 +0530

    Added:Sensus-Analyser

--- main/java/com/bridgelabz/sql/Day29_CensusAnalyser/CSVStateCensus.java
+++ main/java/com/bridgelabz/sql/Day29_CensusAnalyser/CSVStateCensus.java
@@ -8,35 +8,29 @@ import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
 
 public class CSVStateCensus {
-	  public ArrayList<String> readDataFromSource(String path, String expectedColumnHeader, int expectedColumnCount) throws Exception {
-	        ArrayList<String> stateList = new ArrayList<>();
-	        try{
-	            FileReader filereader = new FileReader(path);
-	            int count = 0;
-	            CSVReader csvReader = new CSVReader(filereader);
-	            String[] nextRecord;
-	            while ((nextRecord = csvReader.readNext()) != null) {
-	                int columCount = 0;
-	                String entry = "";
-	                for (String addressBookDetails : nextRecord) {
-	                    entry = entry + addressBookDetails + "\t";
-	                    columCount++;
-	                }
-	                if( count==0 && columCount != expectedColumnCount){
-	                    throw new FileReadException("Error in Reading file");
-	                }
-	                stateList.add(entry);
-	                System.out.println(entry);
-	                count++;
-	            }
-	        } catch (Exception e){
-	            throw new FileReadException("Error in Reading file");
-	        }
+	static String FILE_PATH = "C:\\Day_29_Census_Analyzer\\src\\main\\java\\com\\bridgelabz\\State_census.csv";
 
-	        return stateList;
+    public ArrayList<String> readDataFromSource() throws IOException {
+        ArrayList<String> stateList = new ArrayList<>();
+        try{
+            FileReader filereader = new FileReader(FILE_PATH);
+
+            CSVReader csvReader = new CSVReader(filereader);
+            String[] nextRecord;
+
+            while ((nextRecord = csvReader.readNext()) != null) {
+                String entry = "";
+                for (String addressBookDetails : nextRecord) {
+                    entry = entry + addressBookDetails + "\t";
+                }
+                stateList.add(entry);
+                System.out.println(entry);
+            }
+        } catch (CsvValidationException e){
+           
+        }
+
+        return stateList;
 	    }
-	public ArrayList<String> readDataFromSource() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 }
