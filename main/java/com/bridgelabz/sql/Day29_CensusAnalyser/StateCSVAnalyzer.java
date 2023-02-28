f9716715b5ff540812fa1e087544fbebcf9e3f87
Bharti <bharatifule10gmail.com>
2023-02-28 17:51:40 +0530

    Added:Sensus-Analyser

--- main/java/com/bridgelabz/sql/Day29_CensusAnalyser/StateCSVAnalyzer.java
+++ main/java/com/bridgelabz/sql/Day29_CensusAnalyser/StateCSVAnalyzer.java
@@ -3,13 +3,30 @@ package com.bridgelabz.sql.Day29_CensusAnalyser;
 import java.util.ArrayList;
 
 public class StateCSVAnalyzer {
-	 public static void main(String[] args) {
-         CSVStateCensus stateCensus = new CSVStateCensus();
-         try{
-             ArrayList<String> stateList = stateCensus.readDataFromSource();
-             System.out.println(stateList.size());
-         } catch(Exception e){
-             System.out.println(e.getMessage());
-         }
-    }
+	 static String columnHeaderCensus = "State" + "\t" + "Population"+ "\t" + "AreaInSqKm" + "\t" + "DensityPerSqKm" + "\t";
+
+	    static String columnHeaderStateCode = "SrNo" + "\t" + "StateName" + "\t" + "TIN" + "\t" + "StateCode" + "\t";
+	    public static void main(String[] args) {
+
+	         System.out.println("State Census Data");
+	         System.out.println("_________________");
+	         CSVStateCensus stateCensus = new CSVStateCensus();
+	         try{
+	             ArrayList<String> stateList = stateCensus.readDataFromSource("C:\\Day_29_Census_Analyzer\\src\\main\\java\\com\\bridgelabz\\State_census.csv", columnHeaderCensus,4);
+	         } catch(Exception e){
+	             System.out.println(e.getMessage());
+	         }
+
+	        System.out.println("State Code Data");
+	        System.out.println("_________________");
+	        try{
+	            ArrayList<String> stateList = stateCensus.readDataFromSource("C:\\Day_29_Census_Analyzer\\src\\main\\java\\com\\bridgelabz\\stateCode.csv", columnHeaderStateCode,4);
+	            System.out.println(stateList.size());
+	        } catch(Exception e){
+	            System.out.println(e.getMessage());
+	        }
+	    }
+
+
+
 }
--- test/java/com/bridgelabz/sql/Day29_CensusAnalyser/TestCSVStateCensus.java
+++ test/java/com/bridgelabz/sql/Day29_CensusAnalyser/TestCSVStateCensus.java
@@ -8,55 +8,64 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class TestCSVStateCensus {
-	static CSVStateCensus csvStateCensus;
-    static String columnHeaderCensus = "State Population AreaInSqKm DensityPerSqKm";
-    @Test
-    public void testReadDataFromDestinationHappy(){
-        csvStateCensus =  new CSVStateCensus();
-        try{
-            ArrayList<String> stateList = csvStateCensus.readDataFromSource("C:\\Day_29_Census_Analyzer\\src\\main\\java\\com\\bridgelabz\\State_census.csv", columnHeaderCensus,4);
-            Assertions.assertEquals(stateList.size(), 30);
-        } catch(Exception e){
-            fail("Should not throw exception");
-        }
-    }
+	 static CSVStateCensus csvStateCensus;
+	    static String columnHeaderCensus = "State" + "\t" + "Population"+ "\t" + "AreaInSqKm" + "\t" + "DensityPerSqKm" + "\t";
+	    @Test
+	    public void testReadDataFromDestinationHappy(){
+	        csvStateCensus =  new CSVStateCensus();
+	        try{
+	            ArrayList<String> stateList = csvStateCensus.readDataFromSource("C:\\Day_29_Census_Analyzer\\src\\main\\java\\com\\bridgelabz\\State_census.csv", columnHeaderCensus,4);
+	            Assertions.assertEquals(stateList.size(), 30);
+	        } catch(Exception e){
+	            fail("Should not throw exception");
+	        }
+	    }
 
-    
-    
-    // File name different
-    @Test
-    public void testReadDataFromDestinationSadWrongFile(){
-        csvStateCensus =  new CSVStateCensus();
-        try{
-            ArrayList<String> stateList = csvStateCensus.readDataFromSource("C:\\Day_29_Census_Analyzer\\src\\main\\java\\com\\bridgelabz\\State.csv", columnHeaderCensus,4);
-            fail("Should throw exception");
-        } catch(Exception e){
-            Assertions.assertNotNull(e.getMessage());
-        }
-    }
+	    // File name different
+	    @Test
+	    public void testReadDataFromDestinationSadWrongFile(){
+	        csvStateCensus =  new CSVStateCensus();
+	        try{
+	            ArrayList<String> stateList = csvStateCensus.readDataFromSource("C:\\Day_29_Census_Analyzer\\src\\main\\java\\com\\bridgelabz\\State.csv", columnHeaderCensus,4);
+	            fail("Should throw exception");
+	        } catch(Exception e){
+	            Assertions.assertNotNull(e.getMessage());
+	        }
+	    }
 
-    // Incorrect file extension
-    @Test
-    public void testReadDataFromDestinationSadWrongFormat(){
-        csvStateCensus =  new CSVStateCensus();
-        try{
-            ArrayList<String> stateList = csvStateCensus.readDataFromSource("C:\\Day_29_Census_Analyzer\\src\\main\\java\\com\\bridgelabz\\State.txt", columnHeaderCensus,4);
-            fail("Should throw exception");
-        } catch(Exception e){
-            Assertions.assertEquals(e.getMessage(),"Error in Reading file");
-        }
-    }
+	    // Incorrect file extension
+	    @Test
+	    public void testReadDataFromDestinationSadWrongFormat(){
+	        csvStateCensus =  new CSVStateCensus();
+	        try{
+	            ArrayList<String> stateList = csvStateCensus.readDataFromSource("C:\\Day_29_Census_Analyzer\\src\\main\\java\\com\\bridgelabz\\State.txt", columnHeaderCensus,4);
+	            fail("Should throw exception");
+	        } catch(Exception e){
+	            Assertions.assertEquals(e.getMessage(),"Error in Reading file");
+	        }
+	    }
 
-    // Incorrect delimiter
-    @Test
-    public void testReadDataFromDestinationSadWrongDelimiter(){
-        csvStateCensus =  new CSVStateCensus();
-        try{
-            ArrayList<String> stateList = csvStateCensus.readDataFromSource("C:\\Day_29_Census_Analyzer\\src\\main\\java\\com\\bridgelabz\\State_census_wrong_delimiter.csv", columnHeaderCensus, 4);
-            fail("Should throw exception");
-        } catch(Exception e){
-            Assertions.assertEquals(e.getMessage(), "Error in Reading file");
-        }
-    }
+	    // Incorrect delimiter
+	    @Test
+	    public void testReadDataFromDestinationSadWrongDelimiter(){
+	        csvStateCensus =  new CSVStateCensus();
+	        try{
+	            ArrayList<String> stateList = csvStateCensus.readDataFromSource("C:\\Day_29_Census_Analyzer\\src\\main\\java\\com\\bridgelabz\\State_census_wrong_delimiter.csv", columnHeaderCensus, 4);
+	            fail("Should throw exception");
+	        } catch(Exception e){
+	            Assertions.assertEquals(e.getMessage(), "Error in Reading file");
+	        }
+	    }
+
+	    @Test
+	    public void testReadDataFromDestinationSadWrongHeader(){
+	        csvStateCensus =  new CSVStateCensus();
+	        try{
+	            ArrayList<String> stateList = csvStateCensus.readDataFromSource("C:\\Day_29_Census_Analyzer\\src\\main\\java\\com\\bridgelabz\\State_census_wrong_header.csv", columnHeaderCensus , 4);
+	            fail("Should throw exception");
+	        } catch(Exception e){
+	            Assertions.assertEquals(e.getMessage(), "Error in Reading file");
+	        }
+	    }
 
 }
